c3fb9d1d5dda7d81b2cab5e717589fc80b7a0908
Deybbi Anderson Crisanto Calle <u202120569@upc.edu.pe>
2025-10-29 17:52:19 -0500

    Restore legacy bootstrap entry point

---
+++ src/main/java/com/angeldevs/taskeventservices/TaskEventServicesApplication.java
@@ -0,0 +1,14 @@
+package com.angeldevs.taskeventservices;
+
+import com.angeldevs.taskservice.TaskEventServiceApplication;
+
+public final class TaskEventServicesApplication {
+
+    private TaskEventServicesApplication() {
+        // Utility class
+    }
+
+    public static void main(String[] args) {
+        TaskEventServiceApplication.main(args);
+    }
+}
