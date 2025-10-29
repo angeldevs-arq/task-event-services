c87dda1fbf82feefc6a96776c80c3f098677fe2f
Dacc03 <u202120569@upc.edu.pe>
2025-10-29 16:39:46 -0500

    Initial commit

---
+++ src/main/java/com/angeldevs/taskeventservices/TaskEventServicesApplication.java
@@ -0,0 +1,13 @@
+package com.angeldevs.taskeventservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskEventServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskEventServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angeldevs/taskeventservices/TaskEventServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angeldevs.taskeventservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskEventServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
